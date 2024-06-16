056c37619a7b586e34d7589a798e60309b9b22c3
adlet <eqthyrium@gmamil.c>
2024-06-16 12:12:40 +0600

    first

--- support/RandomInput.go
+++ support/RandomInput.go
@@ -11,9 +11,6 @@ func RandomInitialize() {
 	fmt.Println("Enter the height and width of the map: ")
 	fmt.Scanf("%v %v", &container.Height, &container.Width)
 
-	fmt.Println("Could you give me the amount of the bomb which will be present in the game ?")
-	fmt.Println("Enter the number:")
-	fmt.Scanf("%v", &container.Bomb_amount)
 	container.Initial_Array = make([][]rune, container.Height)
 	container.Data_Array = make([][]int, container.Height)
 	container.State_Array = make([][]bool, container.Height)
@@ -27,10 +24,11 @@ func RandomInitialize() {
 
 		for j := 0; j < container.Width; j++ {
 			randomNumber := rand.Intn(container.Height * container.Width)
-			if randomNumber >= (container.Height*container.Width*3)/10 {
+			if randomNumber >= (container.Height*container.Width*2)/10 {
 				container.Initial_Array[i][j] = '.'
 			} else {
 				container.Initial_Array[i][j] = '*'
+				container.Bomb_amount++
 			}
 
 		}
